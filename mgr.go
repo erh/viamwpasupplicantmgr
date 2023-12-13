7eb9eb90eca81a89eef6f528e8db7fa587212074
Eliot Horowitz <eliothorowitz@gmail.com>
2023-12-13 07:31:08 -0500

    made merging safer

--- mgr.go
+++ mgr.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
 	"go.viam.com/rdk/components/generic"
 	"go.viam.com/rdk/logging"
@@ -12,7 +13,7 @@ import (
 
 var MgrModel = resource.ModelNamespace("erh").WithFamily("viamwpasupplicantmgr").WithModel("manager")
 
-const DefaultPremable string = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n\n"
+const DefaultPremable string = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\n"
 
 func init() {
 	resource.RegisterComponent(
@@ -29,15 +30,27 @@ func newManager(ctx context.Context, deps resource.Dependencies, conf resource.C
 		return nil, err
 	}
 
-	return &mgr{
+	m := &mgr{
 		name: conf.ResourceName(),
 		cfg:  newConf,
-	}, nil
+	}
+
+	_, err = m.checkFileContents()
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
 
 type Credentials struct {
-	SSID string
-	PSK  string
+	SSID    string
+	PSK     string
+	Encoded bool
+}
+
+func (c *Credentials) Equals(other Credentials) bool {
+	return c.SSID == other.SSID && c.PSK == other.PSK && c.Encoded == other.Encoded
 }
 
 type Config struct {
@@ -59,18 +72,25 @@ type mgr struct {
 // return true if file had to change, false if did nothing
 func (m *mgr) checkFileContents() (bool, error) {
 
-	oldBytes, err := os.ReadFile(m.cfg.Filename)
+	preample, oldCreds, err := readFile(m.cfg.Filename)
 	if err != nil {
 		return false, fmt.Errorf("cannot read filename: %s b/c %w", m.cfg.Filename, err)
 	}
+	if m.cfg.Preample == "" {
+		if m.cfg.Preample != "" {
+			m.cfg.Preample = preample
+		} else {
+			m.cfg.Preample = DefaultPremable
+		}
+	}
 
-	oldData := string(oldBytes)
+	m.cfg.Networks = mergeNetwords(oldCreds, m.cfg.Networks)
 
-	newData := m.contents()
-
-	if newData == oldData {
+	if preample == m.cfg.Preample && networksMatch(oldCreds, m.cfg.Networks) {
 		return false, nil
 	}
+
+	newData := m.contents()
 
 	err = os.WriteFile(m.cfg.Filename, []byte(newData), 0666)
 	if err != nil {
@@ -87,7 +107,14 @@ func (m *mgr) contents() string {
 	}
 
 	for _, c := range m.cfg.Networks {
-		s += fmt.Sprintf("network={\n\tssid=\"%s\"\n\tpsk=\"%s\"\n}\n\n", c.SSID, c.PSK)
+		s += "network={\n"
+		s += fmt.Sprintf("\tssid=\"%s\"\n", c.SSID)
+		if c.Encoded {
+			s += fmt.Sprintf("\tpsk=%s\n", c.PSK)
+		} else {
+			s += fmt.Sprintf("\tpsk=\"%s\"\n", c.PSK)
+		}
+		s += "}\n"
 	}
 	return s
 }
@@ -99,4 +126,97 @@ func (m *mgr) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[st
 
 func (m *mgr) Name() resource.Name {
 	return m.name
+}
+
+func readFile(filename string) (string, []Credentials, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", nil, err
+	}
+	return parseFile(string(data))
+}
+
+func parseFile(data string) (string, []Credentials, error) {
+	lines := strings.Split(data, "\n")
+
+	preample := ""
+	creds := []Credentials{}
+
+	inNetwork := false
+
+	for _, l := range lines {
+		l = strings.TrimSpace(l)
+		if l == "" {
+			continue
+		}
+		fmt.Printf("line [%s]\n", l)
+		if inNetwork {
+			if l == "}" {
+				inNetwork = false
+			} else if strings.HasPrefix(l, "ssid=") {
+				l = l[6:]
+				l = l[0 : len(l)-1]
+				creds[len(creds)-1].SSID = l
+			} else if strings.HasPrefix(l, "psk=") {
+				l = l[4:]
+				if l[0] == '"' {
+					l = l[1:]
+					l = l[0 : len(l)-1]
+				} else {
+					creds[len(creds)-1].Encoded = true
+				}
+				creds[len(creds)-1].PSK = l
+			} else {
+				return "", nil, fmt.Errorf("in network and bad line [%s]", l)
+			}
+			continue
+		}
+
+		if strings.HasPrefix(l, "network={") {
+			inNetwork = true
+			creds = append(creds, Credentials{})
+		} else {
+			preample = preample + l + "\n"
+		}
+	}
+	fmt.Printf("preample [%s]\n", preample)
+	return preample, creds, nil
+}
+
+func networksMatch(a, b []Credentials) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for _, x := range a {
+		idx := findNework(b, x.SSID)
+		if idx == -1 {
+			return false
+		}
+		if !x.Equals(b[idx]) {
+			return false
+		}
+	}
+	return true
+}
+
+func findNework(a []Credentials, ssid string) int {
+	for idx, c := range a {
+		if c.SSID == ssid {
+			return idx
+		}
+	}
+	return -1
+}
+
+func mergeNetwords(old, add []Credentials) []Credentials {
+	n := []Credentials{}
+	for _, c := range old {
+		idx := findNework(add, c.SSID)
+		if idx == -1 { // it's not in the new list, so add all
+			n = append(n, c)
+		}
+	}
+	n = append(n, add...)
+	return n
 }
--- mgr_test.go
+++ mgr_test.go
@@ -11,8 +11,9 @@ func TestContents(t *testing.T) {
 	m := mgr{
 		cfg: &Config{
 			Networks: []Credentials{
-				{"a", "b"},
-				{"x", "y"},
+				{"a", "b", false},
+				{"x", "y", false},
+				{"c", "d", true},
 			},
 		},
 	}
@@ -20,19 +21,26 @@ func TestContents(t *testing.T) {
 	s := m.contents()
 	test.That(t, s, test.ShouldEqual, `ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
-
 network={
 	ssid="a"
 	psk="b"
 }
-
 network={
 	ssid="x"
 	psk="y"
 }
-
+network={
+	ssid="c"
+	psk=d
+}
 `,
 	)
+
+	pre, creds, err := parseFile(s)
+	test.That(t, err, test.ShouldBeNil)
+	test.That(t, pre, test.ShouldEqual, DefaultPremable)
+	test.That(t, creds, test.ShouldResemble, m.cfg.Networks)
+
 }
 
 func TestCheckFileContents(t *testing.T) {
@@ -44,8 +52,8 @@ func TestCheckFileContents(t *testing.T) {
 		cfg: &Config{
 			Filename: f.Name(),
 			Networks: []Credentials{
-				{"a", "b"},
-				{"x", "y"},
+				{"a", "b", true},
+				{"x", "y", false},
 			},
 		},
 	}
@@ -57,4 +65,34 @@ func TestCheckFileContents(t *testing.T) {
 	didSomething, err = m.checkFileContents()
 	test.That(t, err, test.ShouldBeNil)
 	test.That(t, didSomething, test.ShouldBeFalse)
+}
+
+func TestHelpers(t *testing.T) {
+	a := []Credentials{
+		{"a", "b", true},
+		{"x", "y", false},
+	}
+	test.That(t, findNework(a, "a"), test.ShouldEqual, 0)
+	test.That(t, findNework(a, "x"), test.ShouldEqual, 1)
+	test.That(t, findNework(a, "m"), test.ShouldEqual, -1)
+
+	b := []Credentials{
+		{"m", "n", true},
+		{"x", "z", false},
+	}
+
+	test.That(t, networksMatch(a, []Credentials{}), test.ShouldBeFalse)
+	test.That(t, networksMatch(a, b), test.ShouldBeFalse)
+	test.That(t, networksMatch(a, a), test.ShouldBeTrue)
+
+	a2 := []Credentials{
+		{"a", "c", true},
+		{"x", "y", false},
+	}
+	test.That(t, networksMatch(a, a2), test.ShouldBeFalse)
+
+	m := mergeNetwords(a, b)
+	test.That(t, len(m), test.ShouldEqual, 3)
+	idx := findNework(m, "x")
+	test.That(t, m[idx].PSK, test.ShouldEqual, "z")
 }
